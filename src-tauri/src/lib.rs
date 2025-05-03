bc24165c0a4c1c5a083fc23f052b272e0fab7ec3
dierbei <1628652790@qq.com>
2025-05-03 16:30:15 +0800

    test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,42 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[derive(serde::Serialize)]
+pub struct Student {
+    pub name: String,
+    pub sex: String,
+    pub age: u32,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn list_student() -> Vec<Student> {
+    vec![
+        Student {
+            name: String::from("张三"),
+            sex: String::from("男"),
+            age: 18,
+        },
+        Student {
+            name: String::from("李四"),
+            sex: String::from("女"),
+            age: 19,
+        },
+        Student {
+            name: String::from("王五"),
+            sex: String::from("男"),
+            age: 20,
+        },
+    ]
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+    .plugin(
+        tauri_plugin_sql::Builder::default()
+            // .add_migrations("sqlite:mydatabase.db", migrations())
+            .build(),
+    )
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![list_student])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
