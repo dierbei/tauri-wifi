d766151aee3f99a5b7d818831bf0f303b2fc0333
dierbei <1628652790@qq.com>
2025-05-03 16:33:32 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 // import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+// import { invoke } from "@tauri-apps/api/core";
 // import "./App.css";
 
 import Database from '@tauri-apps/plugin-sql';
